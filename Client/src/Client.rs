026796d142e4aeef9edf690a316bed993d7bdf8d
melvinRenversez <melvinrenversez003@gmail.com>
2026-02-26 16:26:05 +0100

    get os + get Name device

--- Client/src/Client.rs
+++ Client/src/Client.rs
@@ -4,8 +4,23 @@ use std::io::{Read, Write};
 use std::thread::sleep;
 use std::time::Duration;
 use uuid::Uuid;
+use std::env;
+
+
+
+
+fn device_name() -> String {
+    env::var("COMPUTERNAME")
+        .or_else(|_| env::var("HOSTNAME"))
+        .unwrap_or_else(|_| "unknown".to_string())
+}
+
+
+
 
 fn main() -> std::io::Result<()> {
+
+     println!("OS détecté: {}", env::consts::OS);
 
     let dur = 5;
     
@@ -13,7 +28,7 @@ fn main() -> std::io::Result<()> {
     println!("UUID genere : {}", my_uuid);
     
     loop {       
-        let mut stream = match TcpStream::connect("127.0.0.1:7878") {
+        let mut stream = match TcpStream::connect("172.27.212.88:7878") {
             Ok(s) => s, 
             Err(e) => {
                 println!("Impossible de se connecter : {}.", e);
@@ -31,15 +46,28 @@ fn main() -> std::io::Result<()> {
                 Ok(size) => {
                     if size > 0 {
                         let message = String::from_utf8_lossy(&buffer[..size]);
+                        let message = message.trim();
                         
-                        if message.trim() == "whoareyou" {
+                        if message == "whoareyou" {
                             
                             println!("Whoiam");
                             let uuidBytes = format!("{}\n", my_uuid).into_bytes();
                             let _ = stream.write_all(&uuidBytes);
     
     
-                        }else  {
+                        } else if message == "getOs" {
+                            let os  = env::consts::OS;
+                            let osBytes = format!("{}\n", os).into_bytes();
+                            let _ = stream.write_all(&osBytes);
+
+                        } else if message == "getName" {
+                            println!("getName If");
+                            let name = device_name();
+                            let nameFormated = format!("{}\n", name).into_bytes();
+                            let _ = stream.write_all(&nameFormated);
+                        } 
+
+                        else  {
                             
                             println!("Message recu du server : {}", message);
                         }
--- Server/src/Server.rs
+++ Server/src/Server.rs
@@ -1,70 +1,110 @@
-use std::net::{TcpListener, TcpStream};
-use std::io::{Read, Write};
-use std::sync::{Arc, Mutex};
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
-use std::thread;
-use std::fs;
-use uuid::Uuid;
+use actix_web::{App, HttpResponse, HttpServer, Responder, get, post, web};
 use serde::Serialize;
-use std::io::{BufReader, BufRead};
+use std::fs;
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
+use std::sync::{Arc, Mutex};
+use std::thread;
+use uuid::Uuid;
 
-struct Client{
+struct Client {
     uuid: Option<Uuid>,
+    os: Option<String>,
+    name: Option<String>,
     stream: Arc<Mutex<TcpStream>>,
 }
-
 
 #[derive(Serialize)]
 struct ClientInfo {
     uuid: Option<Uuid>,
+    os: Option<String>,
+    name: Option<String>,
 }
 
 fn handle_client(mut stream: TcpStream, clients: Arc<Mutex<Vec<Client>>>) {
     println!("New Message by {:?}", stream.peer_addr());
 
     let mut buffer = [0; 512];
-    loop{
+    loop {
         match stream.read(&mut buffer) {
             Ok(size) => {
                 if size == 0 {
                     println!("Client desconnected");
-                    
-                    clients.lock().unwrap().retain(|c| c.stream.lock().unwrap().peer_addr().unwrap() != stream.peer_addr().unwrap());
-                    break;
 
-                }else {
+                    clients.lock().unwrap().retain(|c| {
+                        c.stream.lock().unwrap().peer_addr().unwrap() != stream.peer_addr().unwrap()
+                    });
+                    break;
+                } else {
                     let message = String::from_utf8_lossy(&buffer[..size]);
                     println!("Recieves: {}", message);
                 }
             }
             Err(e) => {
                 println!("Error reading: {}", e);
-                clients.lock().unwrap().retain(|c| c.stream.lock().unwrap().peer_addr().unwrap() != stream.peer_addr().unwrap());
+                clients.lock().unwrap().retain(|c| {
+                    c.stream.lock().unwrap().peer_addr().unwrap() != stream.peer_addr().unwrap()
+                });
                 println!("Client restant : {}", clients.lock().unwrap().len());
                 break;
-            } 
+            }
         }
     }
 }
 
-#[get("/")]
-async fn homePage() -> impl Responder  {
+fn getClientData(mut stream: &TcpStream, clients: &Arc<Mutex<Vec<Client>>>) {
+    println!("getClientData");
 
+    stream.try_clone().unwrap().write_all(b"whoareyou");
+    let mut buffer = [0; 64];
+    let size = stream.read(&mut buffer).unwrap();
+    let uuidStr = String::from_utf8_lossy(&buffer[..size]);
+    let clientUuid = Uuid::parse_str(uuidStr.trim()).ok();
+
+    stream.try_clone().unwrap().write_all(b"getOs");
+    let mut buffer = [0; 255];
+    let size = stream.read(&mut buffer).unwrap();
+    let osStr = String::from_utf8_lossy(&buffer[..size]).trim().to_string();
+
+    stream.try_clone().unwrap().write_all(b"getName");
+    let mut buffer = [0; 255];
+    let size = stream.read(&mut buffer).unwrap();
+    let nameStr = String::from_utf8_lossy(&buffer[..size]).trim().to_string();
+
+
+
+    println!("Client uuid : {} , client os : {}, name : {}", uuidStr, osStr, nameStr);
+
+    clients.lock().unwrap().push(Client {
+        uuid: clientUuid,
+        os: Some(osStr),
+        name: Some(nameStr),
+        stream: Arc::new(Mutex::new(stream.try_clone().unwrap())),
+    });
+
+    stream.write_all(b"test data cleitn");
+}
+
+#[get("/")]
+async fn homePage() -> impl Responder {
     let html = fs::read_to_string("src/index.html")
         .unwrap_or_else(|_| "Error: fichier introuvable".to_string());
 
     HttpResponse::Ok()
         .content_type("text/html; charset=utf-8")
         .body(html)
-
 }
 
 #[get("/msg")]
-async fn msg(query: web::Query<std::collections::HashMap<String, String>>, clients: web::Data<Arc<Mutex<Vec<Client>>>>) -> impl Responder {
-    let message = query.get("message")
+async fn msg(
+    query: web::Query<std::collections::HashMap<String, String>>,
+    clients: web::Data<Arc<Mutex<Vec<Client>>>>,
+) -> impl Responder {
+    let message = query
+        .get("message")
         .map(|s| s.to_string())
         .unwrap_or_else(|| "no".to_string());
-    println!( "Received message : {}", message);
+    println!("Received message : {}", message);
 
     let clients_lock = clients.lock().unwrap();
     for mut client in clients_lock.iter() {
@@ -78,40 +118,33 @@ async fn msg(query: web::Query<std::collections::HashMap<String, String>>, clien
 }
 
 #[get("/getDevices")]
-async fn getDevices(clients: web::Data<Arc<Mutex<Vec<Client>>>> ) -> impl Responder {
+async fn getDevices(clients: web::Data<Arc<Mutex<Vec<Client>>>>) -> impl Responder {
     println!("Get Devices");
 
     let clientList = getClientForWeb(&clients);
-   
-   HttpResponse::Ok()
+
+    HttpResponse::Ok()
         .content_type("application/json")
         .body(serde_json::to_string(&clientList).unwrap())
-   
 }
-
-
-
 
 fn clientToInfo(Client: &Client) -> ClientInfo {
     ClientInfo {
         uuid: Client.uuid,
+        os: Client.os.clone(),
+        name: Client.name.clone(),
     }
 }
 
 fn getClientForWeb(clients: &Arc<Mutex<Vec<Client>>>) -> Vec<ClientInfo> {
     let ClientLock = clients.lock().unwrap();
 
-    ClientLock.iter()
-        .map(|c| clientToInfo(c))
-        .collect()
-
+    ClientLock.iter().map(|c| clientToInfo(c)).collect()
 }
 
-
 fn start_tcp_server(clients: Arc<Mutex<Vec<Client>>>) {
-
-        thread::spawn(move || {
-        let listener = match TcpListener::bind("0.0.0.0:7878")  {
+    thread::spawn(move || {
+        let listener = match TcpListener::bind("0.0.0.0:7878") {
             Ok(listener) => listener,
             Err(e) => {
                 println!("Failled to binf TCP {}", e);
@@ -124,21 +157,10 @@ fn start_tcp_server(clients: Arc<Mutex<Vec<Client>>>) {
             match stream {
                 Ok(mut stream) => {
                     println!("New Connexion");
-                    stream.try_clone().unwrap().write_all(b"whoareyou");
 
-                    
-                    let mut reader = BufReader::new(&stream);
-                    let mut uuidStr = String::new();
-                    reader.read_line(&mut uuidStr).unwrap();
-                    let clientUuid = Uuid::parse_str(uuidStr.trim()).ok();
+                    getClientData(&stream, &clients);
 
-                    println!("Client uuid : {}", uuidStr);
-
-                    clients.lock().unwrap().push(Client {
-                        uuid: clientUuid ,
-                        stream: Arc::new(Mutex::new(stream.try_clone().unwrap())),
-                    });
-                    let clients_clone  = clients.clone();
+                    let clients_clone = clients.clone();
                     thread::spawn(move || {
                         handle_client(stream, clients_clone);
                     });
@@ -149,24 +171,21 @@ fn start_tcp_server(clients: Arc<Mutex<Vec<Client>>>) {
     });
 }
 
-
-
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    
     let clients = Arc::new(Mutex::new(Vec::new()));
 
     let tcp_clients = clients.clone();
     thread::spawn(move || {
         start_tcp_server(tcp_clients);
     });
-    
+
     HttpServer::new(move || {
-            App::new()
-                .app_data(web::Data::new(clients.clone()))
-                .service(homePage)
-                .service(msg)
-                .service(getDevices)
+        App::new()
+            .app_data(web::Data::new(clients.clone()))
+            .service(homePage)
+            .service(msg)
+            .service(getDevices)
     })
     .bind(("0.0.0.0", 8000))?
     .run()
