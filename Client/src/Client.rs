66a7f894b2abbeb57a67df0a078994ba754011ce
melvinRenversez <melvinrenversez003@gmail.com>
2026-02-26 14:52:38 +0100

    connection en boucle cote client + mise a  jour de structur object client cote server

--- Client/src/Client.rs
+++ Client/src/Client.rs
@@ -1,12 +1,61 @@
 use std::net::TcpStream;
 use std::io::{Read, Write};
+use std::thread::sleep;
+use std::time::Duration;
+use uuid::Uuid;
 
 fn main() -> std::io::Result<()> {
-    let mut stream = TcpStream::connect("127.0.0.1:7878")?;
-    println!("Connected to the server");
+    
+    let my_uuid = Uuid::new_v4();
+    println!("UUID genere : {}", my_uuid);
+    
+    loop {       
+        let mut stream = match TcpStream::connect("127.0.0.1:7878") {
+            Ok(s) => s, 
+            Err(e) => {
+                println!("Impossible de se connecter : {}.", e);
+                println!("Client Retry in 10s");
+                sleep(Duration::from_secs(10));
+                continue;
+            }
+        };
 
-    stream.write_all(b"hello server");
-    println!("send message");
 
-    Ok(())
+        let mut buffer = [0; 512];
+        loop {
+            println!("Connected successfull");
+            match stream.read(&mut buffer) {
+                Ok(size) => {
+                    if size > 0 {
+                        let message = String::from_utf8_lossy(&buffer[..size]);
+                        
+                        if message.trim() == "whoareyou" {
+                            
+                            println!("Whoiam");
+                            stream.write(my_uuid.to_string().as_bytes());
+    
+    
+                        }else  {
+                            
+                            println!("Message recu du server : {}", message);
+                        }
+    
+                    }
+                }
+                Err(e) => {
+                    println!("Erreur en lecture: {}", e);
+                    break;
+                }
+            }
+    
+        }
+        
+        
+        println!("Client Retry in 10s");
+        sleep(Duration::from_secs(10));
+    }
+
+
+
+
 }
--- Server/src/Server.rs
+++ Server/src/Server.rs
@@ -1,38 +1,132 @@
 use std::net::{TcpListener, TcpStream};
 use std::io::{Read, Write};
+use std::sync::{Arc, Mutex};
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use std::thread;
+use std::fs;
+use uuid::Uuid;
 
+struct Client{
+    uuid: Option<Uuid>,
+    stream: Arc<Mutex<TcpStream>>,
+}
 
-fn handle_client(mut stream: TcpStream) {
+fn handle_client(mut stream: TcpStream, clients: Arc<Mutex<Vec<Client>>>) {
     println!("New Message by {:?}", stream.peer_addr());
 
     let mut buffer = [0; 512];
-    match stream.read(&mut buffer) {
-        Ok(size) => {
-            if size == 0 {
-                println!("Client desconnected");
-            }else {
-                let message = String::from_utf8_lossy(&buffer[..size]);
-                println!("Recieves: {}", message);
+    loop{
+        match stream.read(&mut buffer) {
+            Ok(size) => {
+                if size == 0 {
+                    println!("Client desconnected");
+                    
+                    clients.lock().unwrap().retain(|c| c.stream.lock().unwrap().peer_addr().unwrap() != stream.peer_addr().unwrap());
+                    break;
+
+                }else {
+                    let message = String::from_utf8_lossy(&buffer[..size]);
+                    println!("Recieves: {}", message);
+                }
             }
+            Err(e) => {
+                println!("Error reading: {}", e);
+                clients.lock().unwrap().retain(|c| c.stream.lock().unwrap().peer_addr().unwrap() != stream.peer_addr().unwrap());
+                println!("Client restant : {}", clients.lock().unwrap().len());
+                break;
+            } 
         }
-        Err(e) => println!("Error reading: {}", e),
     }
 }
 
+#[get("/")]
+async fn homePage() -> impl Responder  {
 
-fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:7878")?;
-    println!("Server open on 127.0.0.1:7878...");
+    let html = fs::read_to_string("src/index.html")
+        .unwrap_or_else(|_| "Error: fichier introuvable".to_string());
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => {
-                println!("New Connexion");
-                handle_client(stream);
-            }
-            Err(e) => println!("Connexion failed {}", e),
-        }
+    HttpResponse::Ok()
+        .content_type("text/html; charset=utf-8")
+        .body(html)
+
+}
+
+#[get("/msg")]
+async fn msg(query: web::Query<std::collections::HashMap<String, String>>, clients: web::Data<Arc<Mutex<Vec<Client>>>>) -> impl Responder {
+    let message = query.get("message")
+        .map(|s| s.to_string())
+        .unwrap_or_else(|| "no".to_string());
+    println!( "Received message : {}", message);
+
+    let clients_lock = clients.lock().unwrap();
+    for mut client in clients_lock.iter() {
+        let mut stream = client.stream.lock().unwrap();
+        let _ = stream.write_all(message.as_bytes());
     }
 
-    Ok(())
+    HttpResponse::Found()
+        .append_header(("Location", "/"))
+        .finish()
 }
+
+#[get("/getDevices")]
+async fn getDevices() -> impl Responder {
+    return "getDevices"
+}
+
+
+fn start_tcp_server(clients: Arc<Mutex<Vec<Client>>>) {
+
+        thread::spawn(move || {
+        let listener = match TcpListener::bind("0.0.0.0:7878")  {
+            Ok(listener) => listener,
+            Err(e) => {
+                println!("Failled to binf TCP {}", e);
+                return;
+            }
+        };
+        println!("Server open on 0.0.0.0:7878...");
+
+        for stream in listener.incoming() {
+            match stream {
+                Ok(stream) => {
+                    println!("New Connexion");
+                    clients.lock().unwrap().push(Client {
+                        uuid: None,
+                        stream: Arc::new(Mutex::new(stream.try_clone().unwrap())),
+                    });
+                    let clients_clone  = clients.clone();
+                    thread::spawn(move || {
+                        handle_client(stream, clients_clone);
+                    });
+                }
+                Err(e) => println!("Connexion failed {}", e),
+            }
+        }
+    });
+}
+
+
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    
+    let clients = Arc::new(Mutex::new(Vec::new()));
+
+    let tcp_clients = clients.clone();
+    thread::spawn(move || {
+        start_tcp_server(tcp_clients);
+    });
+    
+    HttpServer::new(move || {
+            App::new()
+                .app_data(web::Data::new(clients.clone()))
+                .service(homePage)
+                .service(msg)
+    })
+    .bind(("0.0.0.0", 8000))?
+    .run()
+    .await
+}
+
+// envoie de message entre server et client avec reponse
